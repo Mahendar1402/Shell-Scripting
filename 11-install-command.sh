f7a2f9296e67a454fe5e60f1a79be97bdb900cf1
Pinapola Mahendar <uaschinnamahesh@gmail.com>
2026-06-13 01:30:35 +0530

    special veriable

--- 11-install-command.sh
+++ 11-install-command.sh
@@ -2,4 +2,12 @@
 USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
     echo "Please run this script as root or with sudo."
+    exit 1
+fi
+dnf install -y figlet
+if [ $? -eq 0 ]; then
+    echo "Figlet installed successfully."
+else
+    echo "Failed to install Figlet."
+    exit 1
 fi
