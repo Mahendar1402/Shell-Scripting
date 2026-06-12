429c30a3e221fbd39d35d83f0426d9290f99d158
Pinapola Mahendar <uaschinnamahesh@gmail.com>
2026-06-13 00:16:14 +0530

    datatype.sh

---
+++ 06-data-types.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+Number1=10
+Number2=20
+echo "The sum of $Number1 and $Number2 is: $((Number1 + Number2))"
+echo "The difference of $Number1 and $Number2 is: $((Number1 - Number2))"
+echo "The product of $Number1 and $Number2 is: $((Number1 * Number2))"
+echo "The quotient of $Number1 and $Number2 is: $((Number1 / Number2))"
+echo "The remainder of $Number1 divided by $Number2 is: $((Number1 % Number2))" 
